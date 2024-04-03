3d4f27e710d1d5a10ed19f393c44f1bf12e37cfe
Your Name <you@example.com>
2024-04-03 08:58:41 -0500

    primer trabajo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <main className='font-roboto flex justify-center items-center'>
+      <h1 className='text-3xl font-bold text-red-700'>Hello word</h1>
+    </main>
+  )
+}
+
+export default App 
